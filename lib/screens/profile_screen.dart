c037c06ef9cce62e58d657f4ff718dd775744dc3
GabrielFGuerreiro <gaferg2004@gmail.com>
2025-03-19 21:39:27 -0300

    base tela de perfis criada

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -9,12 +9,71 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var frameWidth = MediaQuery.of(context).size.width * 0.80;
+    var frameHeight = MediaQuery.of(context).size.height * 0.60;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('', style: TextStyle(color: Colors.white)),
-        backgroundColor: backgroungMainColor,
+      body: Container(
+        alignment: Alignment.center,
+        color: backgroungMainColor,
+        child: Container(
+          width: frameWidth,
+          height: frameHeight,
+          decoration: BoxDecoration(
+            color: backgroundSecondColor,
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+          ),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Profile(name: 'Caio', icon: 'dog'),
+                        Profile(name: 'Hellena', icon: 'uni'),
+                      ],
+                    ),
+                  ),
+                  Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Profile(name: 'Noah', icon: 'robot'),
+                        Profile(name: 'Júlia', icon: 'duck'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Container(color: backgroungMainColor),
+    );
+  }
+}
+
+class Profile extends StatelessWidget {
+  final String name;
+  final String icon;
+  const Profile({super.key, required this.name, required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(100),
+          child: Image.asset('assets/images/icon_$icon.png', width: 150),
+        ),
+        Text(name, style: TextStyle(fontSize: 25, color: Colors.white)),
+      ],
     );
   }
 }
