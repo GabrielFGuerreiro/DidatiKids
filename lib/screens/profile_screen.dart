243f1c322980fa1f77608da4e1537c633f28f8c9
GabrielFGuerreiro <gaferg2004@gmail.com>
2025-03-23 14:28:31 -0300

    att profile_screen - avatar button

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,8 @@
+import 'package:didatikids/components/button_component.dart';
+import 'package:didatikids/screens/add_profile_screen.dart';
 import 'package:didatikids/themes/standart.dart';
 import 'package:flutter/material.dart';
+import 'package:didatikids/utils/form_utils.dart';
 
 class ProfileScreen extends StatelessWidget {
   final String email;
@@ -9,50 +12,69 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
     var frameWidth = MediaQuery.of(context).size.width * 0.80;
     var frameHeight = MediaQuery.of(context).size.height * 0.60;
 
     return Scaffold(
       body: Container(
+        width: width,
+        height: height,
         alignment: Alignment.center,
-        color: backgroungMainColor,
-        child: Container(
-          width: frameWidth,
-          height: frameHeight,
-          decoration: BoxDecoration(
-            color: backgroundSecondColor,
-            borderRadius: BorderRadius.all(Radius.circular(30)),
-          ),
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        color: backgroundMainColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "PERFIS",
+              style: TextStyle(
+                fontSize: 50,
+                fontFamily: "Super Funnel",
+                color: Colors.yellow,
+              ),
+            ),
+            SizedBox(height: 50),
+            Container(
+              width: frameWidth,
+              height: frameHeight,
+              decoration: BoxDecoration(
+                color: backgroundSecondColor,
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+              ),
+              child: Stack(
+                alignment: Alignment.topCenter,
                 children: [
-                  Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Profile(name: 'Caio', icon: 'dog'),
-                        Profile(name: 'Hellena', icon: 'uni'),
-                      ],
-                    ),
-                  ),
-                  Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Profile(name: 'Noah', icon: 'robot'),
-                        Profile(name: 'Júlia', icon: 'duck'),
-                      ],
-                    ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Profile(name: 'Caio', icon: 'dog'),
+                            Profile(name: 'Hellena', icon: 'uni'),
+                          ],
+                        ),
+                      ),
+                      Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Profile(name: 'Noah', icon: 'robot'),
+                            AddProfileButton(),
+                            //Profile(name: 'Júlia', icon: 'duck'),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
@@ -68,12 +90,52 @@ class Profile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(100),
-          child: Image.asset('assets/images/icon_$icon.png', width: 150),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.black, width: 1),
+            borderRadius: BorderRadius.circular(100),
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(100),
+            child: Image.asset('assets/images/icon_$icon.png', width: 150),
+          ),
         ),
-        Text(name, style: TextStyle(fontSize: 25, color: Colors.white)),
+        // SizedBox(height: 5),
+        Text(
+          name,
+          style: TextStyle(
+            fontSize: 25,
+            fontFamily: 'Super Funnel',
+            color: Colors.white,
+          ),
+        ),
       ],
+    );
+  }
+}
+
+class AddProfileButton extends StatelessWidget {
+  const AddProfileButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => AddProfile()),
+        );
+      },
+      child: Container(
+        width: 150,
+        height: 150,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(150, 220, 220, 220),
+          shape: BoxShape.circle,
+        ),
+        child: Icon(Icons.add, size: 70, color: backgroundMainColor),
+      ),
     );
   }
 }
